e986a85459b09aa10bc98255ce1c0eea037ec043
Sam <thesamreaves@gmail.com>
2018-05-05 16:41:33 -0400

    Deck of cards now single array of cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -10,31 +10,33 @@ defmodule Cards do
 
       iex> Cards.create_deck
       [
-  ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
-   "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
-   "Nine of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"],
-  ["Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts",
-   "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts",
-   "Nine of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts"],
-  ["Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs",
-   "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs",
-   "Nine of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs"],
-  ["Ace of Diamonds", "Two of Diamonds", "Three of Diamonds",
-   "Four of Diamonds", "Five of Diamonds", "Six of Diamonds",
-   "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds",
-   "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"]
-]
+         "Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+         "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
+         "Nine of Spades", "Jack of Spades", "Queen of Spades", "King of Spades",
+         "Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts",
+         "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts",
+         "Nine of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts",
+         "Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs",
+         "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs",
+         "Nine of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs",
+         "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds",
+         "Four of Diamonds", "Five of Diamonds", "Six of Diamonds",
+         "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds",
+         "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"
+      ]
 
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
 
-    for suit <- suits do
+    cards = for suit <- suits do
       for value <- values do
         value <> " of " <> suit
       end
     end
+
+    List.flatten(cards)
   end
 
 
