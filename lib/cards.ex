dfb1494df17268367e6250ec35d7de62b74074f4
Sam <thesamreaves@gmail.com>
2018-05-05 17:19:08 -0400

    Cleaned up documentation. Added Save and Load functiosn with first bit of error handling.

--- lib/cards.ex
+++ lib/cards.ex
@@ -8,7 +8,7 @@ defmodule Cards do
 
   ## Examples
 
-      iex> Cards.create_deck
+      iex> Cards.create_deck()
       [
          "Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
          "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
@@ -41,7 +41,7 @@ defmodule Cards do
 
   ## Examples
 
-      iex> Cards.shuffle(Cards.create_deck)
+      iex> Cards.shuffle(Cards.create_deck())
       ["Two", "Ace", "Three"]
 
   """
@@ -51,16 +51,63 @@ defmodule Cards do
 
 
   @doc """
-  Contains
+  Deal
 
   ## Examples
 
-      iex> Cards.contains?(Cards.create_deck, "King")
+      iex> Cards.deal(Cards.create_deck(), 5)
+      ["Two of Hearts", "Ace of Spades", "Three of Clubs", "Jack of Hearts", "Queen of Diamonds"]
+
+  """
+  def deal(deck, size_of_hand) do
+    Enum.split(deck, size_of_hand)
+  end
+
+
+
+  @doc """
+  Contains
+  Checks to see if a card is within the deck
+
+  ## Examples
+
+      iex> Cards.contains?(Cards.create_deck(), "King of The World")
       false
+
+      iex> Cards.contains?(Cards.create_deck(), "King of Hearts")
+      true
 
   """
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+
+  @doc """
+  Save
+  Saves deck of cards to binary
+
+  """
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(binary, file_name)
+  end
+
+
+
+  @doc """
+  Load
+  Loads deck of cards from binary
+
+
+  """
+  def load(file_name) do
+    {status, binary} = File.read(file_name)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "File does not exist"
+    end
   end
 
 
